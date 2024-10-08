a216b14b132e6563a985ce18bae5d69bbdfdcce6
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-10-08 20:53:34 +0100

    Some useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero.tsx";
 import HomeCards from "./components/HomeCards.tsx";
 import JobListings from "./components/JobListings.tsx";
+import ViewAllJobs from "./components/ViewAllJobs.tsx";
 
 const App = () => {
   return (
@@ -10,16 +11,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 };
