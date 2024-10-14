65837f5ad52039a2b9699ff79a29ee30f890cdae
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-10-14 16:48:46 +0100

    Job page - initial

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import MainLayout from './layouts/MainLayout.tsx';
 import JobsPage from './pages/JobsPage.tsx';
 import AddJob from './components/AddJob.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
+import JobPage from './pages/JobPage.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} />
       <Route path='/add-job' element={<AddJob />} />
       <Route path='*' element={<NotFoundPage />} />
 
