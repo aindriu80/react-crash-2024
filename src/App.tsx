96b72b9593a9847030056d48abe3abd3cb93e096
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-10-17 18:35:07 +0100

    Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,26 @@ import NotFoundPage from './pages/NotFoundPage.tsx';
 import JobPage, { jobLoader } from './pages/JobPage.tsx';
 import AddJobPage from './pages/AddJobPage.tsx';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      <Route path='/add-job' element={<AddJobPage />} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-  )
-);
-
 
 const App = () => {
+
+  const addJob = (newJob) => {
+    console.log(newJob)
+
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />
 };
 
